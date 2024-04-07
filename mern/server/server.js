709b5b8182924d12edca8eb8b9fbf140c24076a7
Srujan M Padmashali <142640643+srujanmpadmashalipro@users.noreply.github.com>
2024-04-07 12:33:14 +0530

    Update server.js

--- mern/server/server.js
+++ mern/server/server.js
@@ -5,7 +5,9 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://mern-stack-frontend-ivory.vercel.app/" // Replace with your actual frontend origin
+}));
 app.use(express.json());
 app.use("/record", records);
 
